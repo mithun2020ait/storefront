79327292db9a2e997e72b0eea3ef73de177b565a
Suvendu Chakrabarty <suva123chak@gmail.com>
2022-02-15 08:59:41 -0800

    modifi the project

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def say_hello(request):
+    x = 12
+    y = 2
+    return render(request, 'hello.html', {'name': 'Suvendu'})
